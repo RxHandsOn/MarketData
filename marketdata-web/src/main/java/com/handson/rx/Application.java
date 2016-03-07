e7fdc5ca793bcdda49841ef4f43bea89464e645f
alexvictoor <alexvictoor@gmail.com>
2016-03-07 22:27:39 +0100

    Start all servers using Application main method

--- marketdata-web/src/main/java/com/handson/rx/Application.java
+++ marketdata-web/src/main/java/com/handson/rx/Application.java
@@ -2,6 +2,7 @@ package com.handson.rx;
 
 import com.handson.infra.EventStreamClient;
 import com.handson.infra.RxNettyEventEventStreamClient;
+import rx.schedulers.Schedulers;
 
 import java.io.IOException;
 
@@ -12,8 +13,19 @@ public class Application {
         EventStreamClient forexEventStreamClient = new RxNettyEventEventStreamClient(8096);
         ForexServer forexServer = new ForexServer(8080, forexEventStreamClient);
         forexServer.createServer().start();
+
+        EventStreamClient stockEventStreamClient = new RxNettyEventEventStreamClient(8097);
+
+        StockServer stockServer = new StockServer(8081, stockEventStreamClient, forexEventStreamClient);
+        stockServer.createServer().start();
+
+        EventStreamClient tradeEventStreamClient = new RxNettyEventEventStreamClient(8098);
+        VwapServer vwapServer = new VwapServer(8082, tradeEventStreamClient, Schedulers.immediate());
+        vwapServer.createServer().start();
+
+        System.out.println("Servers ready!");
         System.out.println("Press any key to exit");
-        new RxNettyEventEventStreamClient(8080).readServerSideEvents().toBlocking().forEach(System.out::println);
+        //new RxNettyEventEventStreamClient(8080).readServerSideEvents().toBlocking().forEach(System.out::println);
         System.in.read();
     }
 }
