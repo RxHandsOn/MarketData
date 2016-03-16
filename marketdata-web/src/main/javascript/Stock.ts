71593b93edd89793c168d1b3a5ceb4df847a8aa7
alexvictoor <alexvictoor@gmail.com>
2016-03-16 19:40:54 +0100

    First Typescript exercice

---
+++ marketdata-web/src/main/javascript/Stock.ts
@@ -0,0 +1,23 @@
+import * as rx from '@reactivex/rxjs';
+
+class Quote {
+  constructor(public code:string, public quote:number) {
+  }
+
+  public static parse(json : string) : Quote {
+    return JSON.parse(json);
+  }
+}
+
+function parseRawStream(raw$: rx.Observable<string>) : rx.Observable<Quote>  {
+  return null;
+}
+
+function detectTrends(quote$: rx.Observable<string>) : rx.Observable<string>  {
+  return null;
+}
+
+export {
+  detectTrends,
+  Quote
+}
---
+++ marketdata-web/src/test/javascript/StockTest.ts
@@ -0,0 +1,26 @@
+import * as rx from '@reactivex/rxjs';
+import { expect } from 'chai';
+import * as trends from "../../main/javascript/Stock";
+
+describe("Trends engine", () => {
+
+  it("Should parse a quote", () => {
+    // given
+    const json = "{ \"code\" : \"ibm\" , \"quote\" : 42.34 }"
+    // when
+    const quote = trends.Quote.parse(json);
+    // then
+    expect(quote).to.be.not.null;
+    expect(quote.code).to.be.equal("ibm");
+    expect(quote.quote).to.be.equal(42.34);
+
+  }),
+
+  it("Should detect price getting higher", () => {
+    // TODO
+  }),
+
+  it("Should detect price getting lower", () => {
+    // TODO
+  })
+});
